62268e9480f7f5bf07963ab6330ad1d1634dcb59
anconaesselmann <axel@anconaesselmann.com>
2014-10-31 20:17:14 -0700

    Initial Commit

---
+++ TrustMe4Good/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrustMe4Good
+//
+//  Created by Axel Ancona Esselmann on 10/31/14.
+//  Copyright (c) 2014 Axel Ancona Esselmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
