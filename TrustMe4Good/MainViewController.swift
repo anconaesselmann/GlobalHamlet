2d2ed361af844f9b02a9a0422115090a6bd3a195
anconaesselmann <axel@anconaesselmann.com>
2014-11-03 10:49:42 -0800

    Layout almost functional

---
+++ TrustMe4Good/MainViewController.swift
@@ -0,0 +1,24 @@
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    }
+}
+
