a823bae9470b34331717a1cf92ee5c4a6883ee63
anconaesselmann <axel@anconaesselmann.com>
2014-11-10 18:08:21 -0800

    Some Tests added

--- TrustMe4GoodTests/IdentitySharingViewControllerTest.swift
+++ TrustMe4GoodTests/IdentitySharingViewControllerTest.swift
@@ -3,20 +3,31 @@ import XCTest
 
 class IdentitySharingViewControllerTest: XCTestCase {
     var sut : IdentitySharingViewController!
+    var delegate : InitiateViewController!
     
     override func setUp() {
         super.setUp()
-        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: self.dynamicType))
-        sut = storyboard.instantiateViewControllerWithIdentifier("IdentitySharingViewController") as IdentitySharingViewController
-        sut.loadView()
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func test_prepareForSegue() {
         
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: self.dynamicType))
+        delegate = storyboard.instantiateViewControllerWithIdentifier("InitiateViewController") as InitiateViewController
+        delegate.loadView()
+        delegate.viewDidLoad()
+
+        sut = storyboard.instantiateViewControllerWithIdentifier("IdentitySharingViewController") as IdentitySharingViewController
+        sut.delegate = delegate
+        sut.loadView()
+        sut.viewDidLoad()
+    }
+    
+    func test_switches_connected() {
+        XCTAssertNotNil(sut.switches, "Switches not connected!")
+        var switchNames:[String] = Array<String>()
+        let expectedSwitchNames = ["show_user_name", "show_real_name", "show_alias"]
+        for s in sut.switches {
+            switchNames.append(s.name)
+        }
+        for n in expectedSwitchNames {
+            XCTAssertTrue(contains(switchNames, n))
+        }
     }
 }
