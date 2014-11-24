388658c67acddd41991b84ac77140f89563db7b2
anconaesselmann <axel@anconaesselmann.com>
2014-11-24 15:29:22 -0800

    3d images for launch screen

--- TrustMe4Good/InitialViewController.swift
+++ TrustMe4Good/InitialViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
 
-class InitialViewController: DICNavigationController {    
-
+class InitialViewController: DICNavigationController {
+    
 }
---
+++ TrustMe4Good/ProjectNavigationController.swift
@@ -0,0 +1,8 @@
+/*import UIKit
+
+class ProjectNavigationItem: UINavigationBar {
+    
+    override init(coder) {
+        tintColor = UIColor(red: 255, green: 0, blue: 0, alpha: 50)
+    }
+}*/
